d29e6ca194595a2eb1ef1503af55596f88a3ced2
Joli Holmes <holmesjoli@gmail.com>
2022-04-24 16:41:34 -0400

    updates

--- main.js
+++ main.js
@@ -4,18 +4,11 @@ var graphic = container.select('.scroll__graphic');
 var chart = graphic.select('#chart');
 var text = container.select('.scroll__text');
 var step = text.selectAll('.step');
-
-// define some global vars
+let data = [];
 var chartWidth;
 var chartHeight;
 
-let data = [];
-function updateData() {
-    data = [];
-    for (let i = 0; i < 5; i++) {
-        data.push(Math.random() * 800);
-    }
-}
+updateData();
 var svg = chart.append('svg')
 
 
@@ -32,32 +25,22 @@ var scroller = scrollama();
 
 // resize function to set dimensions on load and on page resize
 function handleResize() {
+    // 1. update height of step elements for breathing room between steps
+    var stepHeight = Math.floor(window.innerHeight * 0.7);
+    step.style('height', stepHeight + 'px');
 
-    if (window.innerHeight >= 700) {
+    // 2. update height of graphic element
+    var bodyWidth = d3.select('body').node().offsetWidth;
 
-        // 1. update height of step elements for breathing room between steps
-        // changing the multiplier here will define how much white space between steps
-        var stepHeight = Math.floor(window.innerHeight * 0.7);
-        step.style('height', stepHeight + 'px');
+    graphic
+        .style('height', window.innerHeight + 'px');
 
-        // 2. update height of graphic element
-        var bodyWidth = d3.select('body').node().offsetWidth;
-        graphic.style('height', window.innerHeight + 'px');
+    // 3. update width of chart by subtracting from text width
+    var chartMargin = 32;
+    var textWidth = text.node().offsetWidth;
 
-        // 3. update width of chart by subtracting from text width
-        var chartMargin = 32;
-        var textWidth = text.node().offsetWidth;
-
-        chartWidth = graphic.node().offsetWidth - textWidth - chartMargin; // left
-
-
-    } else {
-        graphic.style('height', window.innerHeight + 'px');
-        chartWidth = graphic.node().offsetWidth;
-
-        chartHeight = Math.floor(window.innerHeight / 2);
-    }
-
+    // chartWidth = graphic.node().offsetWidth - textWidth - chartMargin; // left
+    chartWidth = graphic.node().offsetWidth; // center
     // make the height 1/2 of viewport
     chartHeight = Math.floor(window.innerHeight / 2);
 
@@ -69,30 +52,46 @@ function handleResize() {
     scroller.resize();
 }
 
+var lastStep;
 // scrollama event handlers
 function handleStepEnter(response) {
+
+    // calculate the direction of scroll
+    let dir;
+    if ((lastStep - response.index) > 0) {
+        dir = 'up'
+    } else {
+        dir = 'down'
+    }
+
+    updateData();
+    if (response.index == 0) {
+        // do nothing on first step
+    } else if (response.index == 1) {
+        fadeCircles(dir);
+    } else if (response.index == 2) {
+        updateChart();
+    } else if (response.index == 3) {
+        newDataForChart(newData)
+    } else {
+        updateChart();
+    }
     // sticky the graphic
     graphic.classed('is-fixed', true);
     graphic.classed('is-bottom', false);
+
 
     // fade in current step
     step.classed('is-active', function (d, i) {
         return i === response.index;
     })
+    console.log("prev: ", lastStep)
+    console.log(response.index)
 
-    console.log(response.index);
-
-    if (response.index === 2) {
-        updateChartStep3()
-    } else {
-        updateData();
-        updateChart();
-    }
-
-    // console.log(response)
+    // store the last step in a global var
+    lastStep = response.index;
 }
 
-// optional to view precise percent progress on callback
 function handleProgress(response) {
     console.log(response)
 }
@@ -111,16 +110,15 @@ function init() {
             text: '.scroll__text', // the step container
             step: '.scroll__text .step', // the step elements
             offset: 0.5, // set the trigger to be 1/2 way down screen
-            debug: true, // display the trigger offset for testing
+            debug: false, // display the trigger offset for testing
             progress: false
         })
-        // .onStepProgress(handleProgress)
+        .onStepProgress(handleProgress)
         .onStepEnter(handleStepEnter);
 
     // setup resize event
     window.addEventListener('resize', handleResize);
 
-    updateData();
     initChart();
 }
 
@@ -131,16 +129,22 @@ init();
 // SOME D3 CODE FOR OUR GRAPHIC //
 /////////////////////////////////
 
+function updateData() {
+    data = [];
+    for (let i = 0; i < 5; i++) {
+        data.push(Math.random() * 800);
+    }
+}
+
 function initChart() {
     // define the width/height of SVG
     svg.attr('width', chartWidth).attr('height', chartHeight);
 
-    // draw the circles
     svg.selectAll('circle')
         .data(data)
         .join('circle')
-        .attr('cy', 100)
-        .attr('r', 40)
+        .attr('cy', chartHeight / 2)
+        .attr('r', 0)
         .attr('fill', 'purple')
         .attr('opacity', 0.7)
         .attr('cx', function (d) {
@@ -148,33 +152,41 @@ function initChart() {
         });
 }
 
+function fadeCircles(dir) {
+    if (dir == 'down') {
+        svg.selectAll('circle').transition().duration(2000).attr('r', 40)
+    } else {
+        svg.selectAll('circle').transition().duration(2000).attr('r', 0)
+    }
+}
+
 function updateChart() {
     svg
         .selectAll('circle')
         .data(data)
         .join('circle')
+        .attr('cy', chartHeight / 2)
         .transition()
-        .duration(2000)
-        .ease(d3.easeExpInOut)
-        .attr("r", 40)
+        .duration(1000)
+        .ease(d3.easeSin)
+        .attr('r', 40)
         .attr('cx', function (d) {
             return d;
         });
 }
 
-function updateChartStep3() {
+function newDataForChart(newData) {
     svg
         .selectAll('circle')
         .data(newData)
         .join('circle')
+        .attr('cy', chartHeight / 2)
         .transition()
-        .duration(2000)
-        .ease(d3.easeExpInOut)
-        .attr('r',function (d) {
-            return d.size;
+        .duration(1000)
+        .ease(d3.easeCubic)
+        .attr('r', function (d) {
+            return d.size * 10;
         })
-        .attr('cx', function (d) {
-            return d.x*10;
-        });
 }
+
 
